587eb784804371c2c18317818ad12d9cfed4462c
Mijorja <115834232+Jorgetr903@users.noreply.github.com>
2026-03-23 21:54:43 +0100

    Cambios

--- frontend/build/web/flutter_service_worker.js
+++ frontend/build/web/flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4b0967d3d3650572105ade78945994cc
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "c1678882cd88248acad1a33ab74283d0",
+"flutter_bootstrap.js": "dbb92225cfb380710d4833274d170017",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -49,7 +49,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4b0967d3d3650572105ade78945994cc
 "manifest.json": "78cd52a97741aeb283df56554d65510e",
 "pdfjs/pdf.mjs": "68b921a0be3132694498930cd8f97290",
 "pdfjs/pdf.worker.mjs": "b13c5259b2013e032b8ac8a6455047ee",
-"pdf_viewer.html": "bfac7a4eb26a6f07c20ccf2b6ab6f7e5",
+"pdf_viewer.html": "9a13ae3c139d45a24f49272a44555a3e",
 "version.json": "5ce5634e8f2f38004089556fd7a9c7cd"};
 // The application shell files that are downloaded before a service worker can
 // start.
